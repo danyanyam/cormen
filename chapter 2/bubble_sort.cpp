ddaba416752182fac2342eb64e7ec385ca9cb5a8
danyanyam <dvbuchko@edu.hse.ru>
2021-12-24 13:12:14 +0300

    added buble sort

---
+++ chapter 2/bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include "utils.h"
+
+template <typename T>
+void bubble_sort(T arr[], int size, bool descending = false) {
+    /**
+     * Function sorts the passsed array of data
+     * with the help of bubble_sort algorithm.
+     *
+     * Complexity is O(n^2)
+     */
+    auto compare = [descending](T lhs, T rhs) {
+        return (descending) ? lhs < rhs : lhs > rhs;
+    };
+
+    for (int i = 0; i < size; i++) {           // n
+        for (int j = size - 1; j >= i; j--) {  // n(n-1)
+            if (compare(arr[i], arr[j])) std::swap(arr[i], arr[j]);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {2, 5, 6, 7, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    bubble_sort(arr, size);
+    print_array(arr, size);  // 1 2 5 6 7
+
+    bubble_sort(arr, size, true);
+    print_array(arr, size);  // 7 6 5 2 1
+}
